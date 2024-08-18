84055e4845a5bedab8c45fa9a9e6fcbef8d1ec48
onyeechris <chrisonyeeofficial@gmail.com>
2024-08-18 02:13:03 +0100

    Adding my kubernetes file

--- index.js
+++ index.js
@@ -7,23 +7,12 @@ const port = process.env.PORT || 3000;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'html')));
 
-// Route to return the current time
-// app.get('/', (req, res) => {
-//   const currentTime = new Date().toLocaleString();
-//   res.json({ currentTime });
-// });
-
-
 // Route to return the current time in bold
 app.get('/time', (req, res) => {
     const currentTime = new Date().toLocaleString();
     res.json({ currentTime: `<b>${currentTime}</b>` });
   });
 
-// Start the server
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
 
 // Start the server
 const start = async () => {
